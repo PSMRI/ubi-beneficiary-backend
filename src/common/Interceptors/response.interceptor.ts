a1cccc4e4186b25426f112721564b6ab1ba50750
Dhanashree-Patil95 <dhanashree.patil@tekditechnologies.com>
2024-11-22 18:39:08 +0530

    Fix[change in interceptor code due to login api fail]

--- src/common/Interceptors/response.interceptor.ts
+++ src/common/Interceptors/response.interceptor.ts
@@ -6,31 +6,34 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
+import { Response } from 'express';
 import { SuccessResponse } from '../responses/success-response';
 import { ErrorResponse } from '../responses/error-response';
 
 @Injectable()
 export class ResponseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const response = context.switchToHttp().getResponse<Response>();
+
     return next.handle().pipe(
       map((data) => {
         if (data instanceof ErrorResponse) {
-          return {
+          response.status(data.statusCode).json({
             statusCode: data.statusCode,
             error: data.errorMessage,
-          }; // Let Nest handle sending this as the response
+          });
         } else if (data instanceof SuccessResponse) {
-          return {
+          response.status(data.statusCode).json({
             statusCode: data.statusCode,
             message: data.message,
             data: data.data,
-          }; // Let Nest handle sending this as the response
+          });
+        } else {
+          return data; // For other response types, pass through without modification
         }
-        return data; // For any unknown type, return as is
       }),
       catchError((err) => {
-        // Pass errors to the global exception handler
-        throw err;
+        throw err; // Re-throw the error to ensure it doesn't get swallowed
       }),
     );
   }
