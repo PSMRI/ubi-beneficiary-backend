6c716d28a896c752a820318f9df49751dd3d1d14
vidya khandekar <vidya.khandekar@tekditechnologies.com>
2025-07-09 11:59:48 +0530

    Task  #243462: Application status cron : Save Status and Comment from JSON String into DB

--- src/entity/user_applications.entity.ts
+++ src/entity/user_applications.entity.ts
@@ -1,43 +1,46 @@
 import {
-  Entity,
-  Column,
-  PrimaryGeneratedColumn,
-  CreateDateColumn,
-  UpdateDateColumn,
+	Entity,
+	Column,
+	PrimaryGeneratedColumn,
+	CreateDateColumn,
+	UpdateDateColumn,
 } from 'typeorm';
 
 @Entity('user_applications')
 export class UserApplication {
-  @Column('uuid')
-  user_id: string;
+	@Column('uuid')
+	user_id: string;
 
-  @Column({ type: 'varchar', length: 255 })
-  benefit_id: string;
+	@Column({ type: 'varchar', length: 255 })
+	benefit_id: string;
 
-  @PrimaryGeneratedColumn('uuid')
-  internal_application_id: string;
+	@PrimaryGeneratedColumn('uuid')
+	internal_application_id: string;
 
-  @Column({ type: 'varchar', length: 255 })
-  benefit_provider_id: string;
+	@Column({ type: 'varchar', length: 255 })
+	benefit_provider_id: string;
 
-  @Column({ type: 'varchar', length: 255 })
-  benefit_provider_uri: string;
+	@Column({ type: 'varchar', length: 255 })
+	benefit_provider_uri: string;
 
-  @Column({ type: 'varchar', length: 100 })
-  external_application_id: string;
+	@Column({ type: 'varchar', length: 100 })
+	external_application_id: string;
 
-  @Column({ type: 'text', nullable: true })
-  application_name: string;
+	@Column({ type: 'text', nullable: true })
+	application_name: string;
 
-  @Column({ type: 'varchar', length: 20 })
-  status: string;
+	@Column({ type: 'varchar', length: 20 })
+	status: string;
 
-  @Column({ type: 'jsonb' })
-  application_data: Record<string, any>;
+	@Column({ type: 'text', nullable: true })
+	remark: string;
 
-  @CreateDateColumn({ type: 'timestamptz' })
-  created_at: Date;
+	@Column({ type: 'jsonb' })
+	application_data: Record<string, any>;
 
-  @UpdateDateColumn({ type: 'timestamptz', nullable: true })
-  updated_at: Date;
+	@CreateDateColumn({ type: 'timestamptz' })
+	created_at: Date;
+
+	@UpdateDateColumn({ type: 'timestamptz', nullable: true })
+	updated_at: Date;
 }
--- src/modules/users/crons/application-status-update.cron.ts
+++ src/modules/users/crons/application-status-update.cron.ts
@@ -9,110 +9,119 @@ import { ProxyService } from '@services/proxy/proxy.service';
 
 @Injectable()
 export class ApplicationStatusUpdate {
-  constructor(
-    @InjectRepository(UserApplication)
-    private readonly userApplicationRepository: Repository<UserApplication>,
-    private readonly configService: ConfigService,
-    private readonly proxyService: ProxyService,
-  ) { }
+	constructor(
+		@InjectRepository(UserApplication)
+		private readonly userApplicationRepository: Repository<UserApplication>,
+		private readonly configService: ConfigService,
+		private readonly proxyService: ProxyService,
+	) {}
 
-  async getApplications() {
-    try {
-      const applications = await this.userApplicationRepository.find({
-        where: {
-          status: Not(In(['amount received', 'rejected', 'disbursed'])),
-        },
-      });
+	async getApplications() {
+		try {
+			const applications = await this.userApplicationRepository.find({
+				where: {
+					status: Not(In(['amount received', 'rejected', 'disbursed'])),
+				},
+			});
 
-      return applications;
-    } catch (error) {
-      Logger.error(`Error while getting user applications: ${error}`);
-      return [];
-    }
-  }
+			return applications;
+		} catch (error) {
+			Logger.error(`Error while getting user applications: ${error}`);
+			return [];
+		}
+	}
 
-  async updateStatus(application: any, status: string) {
-    try {
-      if (!status) return;
-      application.status = status.toLocaleLowerCase();
+	async updateStatus(
+		application: any,
+		statusData: { status: string; comment: string },
+	) {
+		try {
+			if (!statusData?.status) return;
 
-      const queryRunner =
-        this.userApplicationRepository.manager.connection.createQueryRunner();
-      await queryRunner.connect();
-      try {
-        await queryRunner.startTransaction();
-        await queryRunner.manager.save(application);
-        await queryRunner.commitTransaction();
-      } catch (error) {
-        await queryRunner.rollbackTransaction();
-        Logger.error(`Error in query runner: ${error}`);
-      } finally {
-        await queryRunner.release();
-      }
-    } catch (error) {
-      Logger.error(`Error while updating application status: ${error}`);
-    }
-  }
+			application.status = statusData.status.toLowerCase(); // e.g., "approved"
+			application.remark = statusData.comment || ''; // Save the comment
 
-  async getStatus(orderId: string) {
-    const body = {
-      context: {
-        domain: 'onest:financial-support',
-        action: 'status',
-        timestamp: new Date().toISOString(),
-        ttl: 'PT10M',
-        version: '1.1.0',
-        bap_id: this.configService.get<string>('BAP_ID'),
-        bap_uri: this.configService.get<string>('BAP_URI'),
-        bpp_id: this.configService.get<string>('BPP_ID'),
-        bpp_uri: this.configService.get<string>('BPP_URI'),
-        transaction_id: uuidv4(),
-        message_id: uuidv4(),
-      },
-      message: {
-        order_id: orderId,
-      },
-    };
+			const queryRunner =
+				this.userApplicationRepository.manager.connection.createQueryRunner();
+			await queryRunner.connect();
+			try {
+				await queryRunner.startTransaction();
+				await queryRunner.manager.save(application);
+				await queryRunner.commitTransaction();
+			} catch (error) {
+				await queryRunner.rollbackTransaction();
+				Logger.error(`Error in query runner: ${error}`);
+			} finally {
+				await queryRunner.release();
+			}
+		} catch (error) {
+			Logger.error(`Error while updating application status: ${error}`);
+		}
+	}
 
-    const response = await this.proxyService.bapCLientApi2('status', body);
+	async getStatus(orderId: string) {
+		const body = {
+			context: {
+				domain: 'onest:financial-support',
+				action: 'status',
+				timestamp: new Date().toISOString(),
+				ttl: 'PT10M',
+				version: '1.1.0',
+				bap_id: this.configService.get<string>('BAP_ID'),
+				bap_uri: this.configService.get<string>('BAP_URI'),
+				bpp_id: this.configService.get<string>('BPP_ID'),
+				bpp_uri: this.configService.get<string>('BPP_URI'),
+				transaction_id: uuidv4(),
+				message_id: uuidv4(),
+			},
+			message: {
+				order_id: orderId,
+			},
+		};
 
-    try {
-      const status =
-        response?.responses[0]?.message?.order?.fulfillments[0]?.state
-          ?.descriptor?.name;
+		const response = await this.proxyService.bapCLientApi2('status', body);
 
-      return status;
-    } catch (error) {
-      console.error(`Error while getting status from response: ${error}`);
-      throw new Error('Error while getting status from response');
-    }
-  }
+		try {
+			const rawStatus =
+				response?.responses[0]?.message?.order?.fulfillments[0]?.state
+					?.descriptor?.name;
 
-  async processApplications(applications: any) {
-    try {
-      await Promise.all(
-        applications.map(async (application: any) => {
-          const status = await this.getStatus(
-            application.external_application_id,
-          );
-          await this.updateStatus(application, status);
-        }),
-      );
-    } catch (error) {
-      Logger.error(`Error while processing applications: ${error}`);
-    }
-  }
+			if (!rawStatus) return null;
 
-  @Cron('*/15 * * * *')
-  async updateApplicationStatusCron() {
-    try {
-      // Get user application records from databse
-      const applications = await this.getApplications();
+			// Parse status stringified JSON
+			const parsedStatus = JSON.parse(rawStatus);
+			return parsedStatus; // { status: '...', comment: '...' }
+		} catch (error) {
+			console.error(`Error while getting status from response: ${error}`);
+			throw new Error('Error while getting status from response');
+		}
+	}
 
-      // Update status of each application
-      await this.processApplications(applications);
-    } catch (error) {
-      Logger.error(`Error in 'Update user application cron': ${error}`);
-    }
-  }
+	async processApplications(applications: any) {
+		try {
+			await Promise.all(
+				applications.map(async (application: any) => {
+					const statusData = await this.getStatus(
+						application.external_application_id,
+					);
+					await this.updateStatus(application, statusData);
+				}),
+			);
+		} catch (error) {
+			Logger.error(`Error while processing applications: ${error}`);
+		}
+	}
+
+	@Cron('*/1 * * * *')
+	async updateApplicationStatusCron() {
+		try {
+			// Get user application records from databse
+			const applications = await this.getApplications();
+
+			// Update status of each application
+			await this.processApplications(applications);
+		} catch (error) {
+			Logger.error(`Error in 'Update user application cron': ${error}`);
+		}
+	}
 }
