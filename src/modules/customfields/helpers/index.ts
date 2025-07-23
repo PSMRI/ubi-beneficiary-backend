e0685692848a709fb1c0ef62595acee66dac5714
Rajnish Dargan <rajnish.code30@gmail.com>
2025-07-23 20:16:34 +0530

    Issue #243256 fix: Removed unused files

--- src/modules/customfields/helpers/index.ts
+++
@@ -1,3 +0,0 @@
-export { FieldValidationService } from './field-validation.service';
-export { FieldEncryptionService } from './field-encryption.service';
-export type { ValidationResult, ValidationOptions } from './field-validation.service';
