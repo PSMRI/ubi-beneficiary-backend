4d231de74f1143ceeb85f001b79e1991a2471577
vidya khandekar <vidya.khandekar@tekditechnologies.com>
2025-09-23 00:26:38 +0530

    Task #247098 feat: Update ContentModule to include NetworkCache entity and forward reference to UserModule, enhancing module dependencies and export capabilities.

--- src/content/content.module.ts
+++ src/content/content.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { LoggerService } from 'src/logger/logger.service';
 import { HasuraService } from 'src/services/hasura/hasura.service';
@@ -7,14 +7,15 @@ import { ContentController } from './content.controller';
 import { ContentService } from './content.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ResponseCache } from 'src/entity/response.entity';
+import { NetworkCache } from 'src/entity/network-cache.entity';
 import { EncryptionService } from 'src/common/helper/encryptionService';
 import { UserModule } from '../modules/users/users.module';
 import { NetworkCacheRefreshCron } from './network-cache-refresh.cron';
 import { AuthModule } from '../modules/auth/auth.module';
-
 @Module({
-  imports: [ScheduleModule.forRoot(), TypeOrmModule.forFeature([ResponseCache]), UserModule, AuthModule],
+  imports: [ScheduleModule.forRoot(), TypeOrmModule.forFeature([ResponseCache, NetworkCache]), forwardRef(() => UserModule), AuthModule],
   controllers: [ContentController],
-  providers: [ContentService, HasuraService, ProxyService, LoggerService, EncryptionService, NetworkCacheRefreshCron]
+  providers: [ContentService, HasuraService, ProxyService, LoggerService, EncryptionService, NetworkCacheRefreshCron],
+  exports: [ContentService]
 })
 export class ContentModule {}
