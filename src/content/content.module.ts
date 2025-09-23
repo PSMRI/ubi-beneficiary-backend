4da00dfe983bf3bc5ac34e38f22329f981304c3e
vidya khandekar <vidya.khandekar@tekditechnologies.com>
2025-09-23 17:48:59 +0530

    Task #247098 feat: Update ContentModule to remove NetworkCache entity from imports, streamlining module dependencies and improving code clarity.

--- src/content/content.module.ts
+++ src/content/content.module.ts
@@ -7,15 +7,15 @@ import { ContentController } from './content.controller';
 import { ContentService } from './content.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ResponseCache } from 'src/entity/response.entity';
-import { NetworkCache } from 'src/entity/network-cache.entity';
+
 import { EncryptionService } from 'src/common/helper/encryptionService';
 import { UserModule } from '../modules/users/users.module';
 import { NetworkCacheRefreshCron } from './network-cache-refresh.cron';
 import { AuthModule } from '../modules/auth/auth.module';
 @Module({
-  imports: [ScheduleModule.forRoot(), TypeOrmModule.forFeature([ResponseCache, NetworkCache]), forwardRef(() => UserModule), AuthModule],
+  imports: [ScheduleModule.forRoot(), TypeOrmModule.forFeature([ResponseCache]), UserModule, AuthModule],
   controllers: [ContentController],
   providers: [ContentService, HasuraService, ProxyService, LoggerService, EncryptionService, NetworkCacheRefreshCron],
-  exports: [ContentService]
+
 })
 export class ContentModule {}
