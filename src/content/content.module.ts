a04ba9bd7ef52c41df7ed76252fb5d3e295ad78f
vidya khandekar <vidya.khandekar@tekditechnologies.com>
2025-09-23 20:50:19 +0530

    Task #247098 fix: Clean up ContentModule by removing unnecessary whitespace, enhancing code clarity and maintainability.

--- src/content/content.module.ts
+++ src/content/content.module.ts
@@ -11,10 +11,10 @@ import { EncryptionService } from 'src/common/helper/encryptionService';
 import { UserModule } from '../modules/users/users.module';
 import { NetworkCacheRefreshCron } from './network-cache-refresh.cron';
 import { AuthModule } from '../modules/auth/auth.module';
+
 @Module({
   imports: [ScheduleModule.forRoot(), TypeOrmModule.forFeature([ResponseCache]), UserModule, AuthModule],
   controllers: [ContentController],
   providers: [ContentService, HasuraService, ProxyService, LoggerService, EncryptionService, NetworkCacheRefreshCron]
-
 })
 export class ContentModule {}
