15432bc7d0313ebd40e8edffde10dc5ed6511ba0
vidya khandekar <vidya.khandekar@tekditechnologies.com>
2025-11-10 12:06:55 +0530

    Task #248611 feat: Updated DTO and service methods for oldPassword field

--- src/modules/auth/dto/update-password.dto.ts
+++ src/modules/auth/dto/update-password.dto.ts
@@ -7,6 +7,10 @@ export class UpdatePasswordDTO {
 
     @IsNotEmpty()
     @IsString()
+    oldPassword: string;
+
+    @IsNotEmpty()
+    @IsString()
     @MinLength(8)
     newPassword: string;
 }
