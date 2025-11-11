0566e3be307b3afa0dbbe051c97b06893deca7b2
vidya khandekar <vidya.khandekar@tekditechnologies.com>
2025-11-11 11:16:31 +0530

    Task #248910 feat: Add endpoint for processing OTR certificates with file upload support

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,22 +1,30 @@
 import {
   Body,
   Controller,
+  HttpCode,
+  HttpStatus,
   Post,
   Req,
   Res,
+  UploadedFile,
+  UseInterceptors,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBody, ApiResponse, ApiTags, ApiConsumes } from '@nestjs/swagger';
 import { Request, Response } from 'express';
 import { AuthService } from './auth.service';
 import { RegisterDTO } from './dto/register.dto';
 import { LoginDTO } from './dto/login.dto';
+import { SuccessResponse } from 'src/common/responses/success-response';
+import { ErrorResponse } from 'src/common/responses/error-response';
+import { UploadDocumentDto } from '@modules/users/dto/upload-document.dto';
 
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService) { }
 
   // users/register on keycloak and postgres both side.
 
@@ -49,6 +57,34 @@ export class AuthController {
   public async login(@Body() body: LoginDTO) {
     return await this.authService.login(body);
   }
+
+  @Post('process-otr-certificate')
+  @HttpCode(HttpStatus.OK)
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+        docType: { type: 'string' },
+        docSubType: { type: 'string' },
+        docName: { type: 'string' },
+        importedFrom: { type: 'string' },
+      },
+    },
+  })
+  async processOtrCertificate(
+    @Req() req: any,
+    @UploadedFile() file: Express.Multer.File,
+    @Body() uploadDocumentDto: UploadDocumentDto,
+  ): Promise<SuccessResponse | ErrorResponse> {
+    return this.authService.processOtrCertificate(req, file, uploadDocumentDto);
+  }
+
 
   @Post('/logout')
   @UsePipes(ValidationPipe)
